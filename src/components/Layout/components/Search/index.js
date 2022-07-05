fec745e40be5f55d5ea5851c6c92a61e3bc16884
NguyenHoAnhLam <nguyenhoanhlam161120000@gmial.com>
2022-07-06 00:08:23 +0700

    Tìm hiểu và sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -3,6 +3,8 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
+
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as WrapperPopper } from '~/components/Popper';
 import AccountItem from '~/components/Layout/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
@@ -27,20 +29,16 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const featchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`).then(
-            (res) =>
-                res
-                    .json()
-                    .then((res) => {
-                        setSearchResult(res.data);
-                        setLoading(false);
-                    })
-                    .catch(() => {
-                        setLoading(false);
-                    }),
-        );
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        featchApi();
     }, [debounced]);
 
     //handle logic
